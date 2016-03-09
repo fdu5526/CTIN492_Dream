517eaecfc83adc3d2d1e2b23f289450880f024ae
Dave Yan <fdu5526@live.com>
2016-03-08 16:13:03 -0800

    added door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnTriggerExit (Collider other) {
+		GetComponent<Collider>().enabled = true;
+		GetComponent<Renderer>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
