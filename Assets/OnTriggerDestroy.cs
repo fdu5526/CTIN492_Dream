d06ff73aa04aacc5c2a256b0cedb99ba63a6742d
Dave Yan <fdu5526@live.com>
2016-03-08 20:04:31 -0800

    cities yay

---
+++ Assets/OnTriggerDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTriggerDestroy : MonoBehaviour {
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerExit (Collider other) {
+		Destroy(target);
+	}
+}
